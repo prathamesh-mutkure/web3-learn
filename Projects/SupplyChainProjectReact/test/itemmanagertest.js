8693f3a151daa32a387fd09e5e5f928953182b2a
Prathamesh Mutkure <pmutkure009@gmail.com>
2022-06-03 12:07:10 +0530

    Added Comments

--- Projects/SupplyChainProjectReact/test/itemmanagertest.js
+++ Projects/SupplyChainProjectReact/test/itemmanagertest.js
@@ -13,6 +13,7 @@ contract("ItemManager", (accounts) => {
     const { index, itemAddress } = result.logs[0].args;
     const itemIndex = index.toNumber();
 
+    // Checking for the item to be the first in that mapping
     assert.equal(itemIndex, 0, "This isn't the first item");
 
     const item = await itemManagerInstance.items(itemIndex);
@@ -20,3 +21,9 @@ contract("ItemManager", (accounts) => {
     assert.equal(item.identifier, itemName, "The identifer doesn't match");
   });
 });
+
+// Truffle re-deploys the smart contract for testing purpose
+// Thus, the smart contract is empty
+// Atleast, for the firt test case
+// Truffle discards the address at which the contract is stored at
+// Thus, it is lost forever
